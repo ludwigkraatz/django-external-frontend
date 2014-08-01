2b8cf704f64ce098e498f594803179dcb54be545
Ludwig Kraatz <ludwig.kraatz@suncircle.de>
2014-08-02 00:31:58 +0200

    update version

--- setup.py
+++ setup.py
@@ -12,10 +12,10 @@ for requirement in parse_requirements('requirements.txt'):
 
 
 setup(
-    name="external_frontend",
+    name="external-frontend",
     author="Ludwig Kraatz",
     author_email="code@suncircle.de",
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     include_package_data=True,
     install_requires=requirements,
