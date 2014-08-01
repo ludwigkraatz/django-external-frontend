cc3cb532e7be8944aed35f250ee0e86023e48650
Ludwig Kraatz <ludwig.kraatz@suncircle.de>
2014-08-02 01:42:35 +0200

    updated setup.py & dependencies
    fixed indentation bug

--- external_frontend/utils/watch.py
+++ external_frontend/utils/watch.py
@@ -1,9 +1,8 @@
 
 
-
 def watch_folder(builder, stdout=None, stdin=None):
     # TODO: stdin = sys.stdin.read() if stdin else None
-
+    pass
 
 
 def watch_git(mapper, stdout=None, stderr=None, stdin=None):
