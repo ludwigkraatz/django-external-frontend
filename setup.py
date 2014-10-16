872afb79330ff85f2dc20ef1828222e3430a660a
Ludwig Kraatz <ludwig.kraatz@suncircle.de>
2014-10-17 00:59:04 +0200

    updated dependencies & version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     name="external-frontend",
     author="Ludwig Kraatz",
     author_email="code@suncircle.de",
-    version='0.1.33',
+    version='0.1.34',
     packages=find_packages(),
     package_data={'external_frontend': ['external_frontend/frontends']},
     include_package_data=True,
